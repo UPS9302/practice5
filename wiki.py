84c7c32361f3b81e373145517e895f776f9552f2
UPS9302 <ujjwal20098.dpskalyanpur@gmail.com>
2025-10-18 11:03:10 +0000

    "number of sentences"

--- wiki.py
+++ wiki.py
@@ -3,13 +3,14 @@ import wikipedia
 # Set language
 n=input("enter language code en/de/fr/hi:")
 wikipedia.set_lang(n)
+n2=int(input("Enter no. of sentences for summary:"))
 
 # Search query
 query = input("Enter a topic to search on Wikipedia: ")
 
 # Get summary
 try:
-    summary = wikipedia.summary(query, sentences=3)
+    summary = wikipedia.summary(query, sentences=n2)
     print("\nWikipedia Summary:")
     print(summary)
 except wikipedia.exceptions.PageError:
